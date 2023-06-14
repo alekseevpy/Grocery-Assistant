3f68aeff5b4ba308cddb91975f60929044c61b68
Лев Алексеев <futuristjp@gmail.com>
2023-06-15 00:23:09 +0500

    Сделал все правки после первого ревью

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,14 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import IngredientViewSet, RecipeViewSet, TagViewSet
+from .views import (
+    IngredientViewSet,
+    RecipeViewSet,
+    TagViewSet,
+    FavoriteViewSet,
+    ShoppingCartViewSet,
+    DownloadShoppingCartView,
+)
 
 app_name = "api"
 router = DefaultRouter()
@@ -10,5 +17,25 @@ router.register(r"ingredients", IngredientViewSet, basename="ingredients")
 router.register(r"recipes", RecipeViewSet, basename="recipes")
 
 urlpatterns = [
+    path(
+        "recipes/<int:recipe_id>/favorite/",
+        FavoriteViewSet.as_view(),
+        name="favorite_recipe",
+    ),
+    path(
+        "recipes/<int:recipe_id>/shopping_cart/",
+        ShoppingCartViewSet.as_view(),
+        name="shopping_cart",
+    ),
+    path(
+        "recipes/<int:recipe_id>/favorite/",
+        FavoriteViewSet.as_view(),
+        name="favorite_recipe",
+    ),
+    path(
+        "recipes/download_shopping_cart/",
+        DownloadShoppingCartView.as_view(),
+        name="download_shopping_cart",
+    ),
     path("", include(router.urls)),
 ]
